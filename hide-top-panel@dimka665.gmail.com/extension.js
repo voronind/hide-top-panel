4b1b8c8fba579c71a28c2538c62dfc2f7e4a6be1
dimka665 <dimka665@gmail.com>
2013-11-08 04:51:18 +0400

    refactoring to fix in Ubuntu GNOME 13.10 Shell 3.8.4

--- hide-top-panel@dimka665.gmail.com/extension.js
+++ hide-top-panel@dimka665.gmail.com/extension.js
@@ -2,27 +2,27 @@
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
 
-const panel_actor = Main.panel.actor;
-const panel_box = panel_actor.get_parent();
+const panel = Main.panel.actor;
+const panel_box = panel.get_parent();
 
-let panel_height = panel_actor.get_height();
+let panel_height = panel.get_height();
 
 let show_event = false;
 let hide_event = false;
 
-let ANIMATION_SPEED = 0.4;
+let ANIMATION_SPEED = 0.45;
 
 
 function hide_panel() {
-    Tweener.addTween(panel_actor, {
-        translation_y: -panel_height,
+    Tweener.addTween(panel, {
+	translation_y: 0,
         time: ANIMATION_SPEED
     });
 }
 
 function show_panel() {
-    Tweener.addTween(panel_actor, {
-        translation_y: 0,
+    Tweener.addTween(panel, {
+        translation_y: panel_height,
         time: ANIMATION_SPEED
     });
 }
@@ -34,7 +34,7 @@ function enable() {
     show_event = Main.overview.connect('showing', show_panel);
     hide_event = Main.overview.connect('hiding', hide_panel);
 
-    panel_box.height = 0;
+    panel_box.set_position(0, -panel_height);
     hide_panel();
 }
 
@@ -42,6 +42,8 @@ function disable() {
     if(show_event) Main.overview.disconnect(show_event);
     if(hide_event) Main.overview.disconnect(hide_event);
 
-    panel_box.height = panel_height;
-    show_panel();
+    panel_box.set_position(0, 0);
+    panel.translation_y = 0;
 }
+
+
